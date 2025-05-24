ca5e599922877f6f6e6bd70588d2ca4857176c09
ashu <ashutoshvishwakarma222@gmail.com>
2025-05-24 18:00:32 +0530

    video-reels-component

--- src/components/VideoReel.jsx
+++ src/components/VideoReel.jsx
@@ -1,6 +1,23 @@
 import React from 'react';
+import "./VideoReel.css";
+
 function VideoReel() {
-    return ( <h1>Video Reel</h1> );
+    return ( 
+        <div className='container-fluid' style={{backgroundColor: "#F9E5DA"}}>
+            <div className='row'>
+                <div className='col play'>
+                    <img src='assets/video.png' alt='video-tab'/>
+                    <img src='assets/time.png' alt='time' id='time'/>
+                    <p><i class="fa-light fa-circle-play"></i></p>
+                </div>
+                <div className='col desc'>
+                    <h5>VIDEO REEL</h5>
+                    <h3>Unlock The Greatest<br/> Value Possible</h3>
+                    <p>Design, business understanding, ability to put themselves<br/> in the merchant's shoes meant to partner.</p>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default VideoReel;
